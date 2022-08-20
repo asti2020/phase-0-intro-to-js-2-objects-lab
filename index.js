e305774acfea69a553e58a6f0e05676610be14b0
Astika Luintel <astild2020@gmail.com>
2022-08-20 18:07:50 -0400

    commit tree 00ec31465b0eeb054f3d16de5d8bfe260ddcc8a2

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {};
+employee.name = "sita";
+employee.streetAddress = "21 Broadway";
+
+function updateEmployeeWithKeyAndValue(){
+    let newEmployee = {...employee };
+    newEmployee["name"] = "Sam";
+    newEmployee["streetAddress"] = "11 Broadway";
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+   employee.name = "Sam";
+   employee.streetAddress = "12 Broadway";
+    return employee; 
+}
+
+function deleteFromEmployeeByKey(){
+    let borEmploy = {...employee};
+    delete borEmploy .name;
+    borEmploy.age = "33";
+   borEmploy.streetAddress = "13 Broadway";
+   return borEmploy;
+
+};
+
+function destructivelyDeleteFromEmployeeByKey(){
+
+    delete employee.name;
+    employee.age = "33";
+   employee.streetAddress = "13 Broadway";
+   return employee;
+};
